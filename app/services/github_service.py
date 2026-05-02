e9039bafb35236576df844e30f4c87cd4b593d62
firedotguy <nta16022013@gmail.com>
2026-05-02 14:24:09 +0300

    chore: sort projects by stars

--- app/services/github_service.py
+++ app/services/github_service.py
@@ -4,7 +4,13 @@ from github.Repository import Repository
 g = Github()
 
 def get_projects() -> list[Repository]:
-    return g.get_organization('itd-sdk').get_repos(sort='updated').get_page(0) # bruh how can i sort by stars stupid lib
+    repos = g.get_organization('itd-sdk').get_repos().get_page(0) # bruh how can i sort by stars stupid lib
+    repos.sort(key=lambda repo: repo.stargazers_count, reverse=True)
+    try:
+        repos.remove(next((repo for repo in repos if repo.name == '.github')))
+    except StopIteration:
+        pass
+    return repos
 
 def get_analogs() -> list[Repository]:
     return [g.get_repo('gam5510/itdpy'), g.get_repo('EpsilonRationes/aioitd'), g.get_repo('IRRatium/itdirr')]
