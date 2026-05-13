e95cabd60145e13ad1f1e81c2e02cd906218b277
fi-res <nta16022013@gmail.com>
2026-05-13 17:03:54 +0300

    fix(login): add image checking before clciking

--- app/services/login.py
+++ app/services/login.py
@@ -1,8 +1,24 @@
-from time import sleep
+from time import sleep, time
 from subprocess import Popen, PIPE
 from app.logger import get_logger
 
+from pyautogui import click, typewrite, hotkey, locateOnScreen as locate_on_screen, ImageNotFoundException
+from pyperclip import paste
+
 l = get_logger('services.login')
+
+def wait_for_image(path: str, timeout: int = 30):
+    path = f'login_screens/{path}'
+    start = time()
+    while True:
+        sleep(0.2)
+        if time() - start > timeout:
+            l.warning(f'{path} not found after timeout')
+            break
+        try:
+            return locate_on_screen(path, grayscale=True)
+        except ImageNotFoundException:
+            pass
 
 def login(email: str, password: str) -> str | None:
     # start browser
@@ -12,11 +28,9 @@ def login(email: str, password: str) -> str | None:
     # click(856, 813)
     # sleep(2)
     try:
-        from pyautogui import click, typewrite, hotkey
-        from pyperclip import paste
 
-        firefox = Popen(['firefox', '--private-window', 'https://xn--d1ah4a.com/'], stdout=PIPE, stderr=PIPE)
-        sleep(8)
+        Popen(['firefox', '--private-window', 'https://xn--d1ah4a.com/'], stdout=PIPE, stderr=PIPE)
+        assert wait_for_image('home.png')
 
         # login
         l.debug('enter email')
@@ -31,13 +45,12 @@ def login(email: str, password: str) -> str | None:
 
         l.debug('click login button')
         click(802, 621)
-        sleep(8)
+        wait_for_image('captcha.png', 15)
 
         l.debug('click captcha')
         click(638, 440)
-        sleep(5)
+        wait_for_image('logout.png')
 
-        # token spizdet
         l.debug('open devtools')
         hotkey('f12')
         sleep(0.5)
@@ -46,19 +59,20 @@ def login(email: str, password: str) -> str | None:
         click(769, 103)
         sleep(0.5)
 
-        l.debug('copy token')
-        click(406, 306, clicks=2)
-        sleep(0.5)
+        if location := wait_for_image('token.png', 1):
+            l.debug('copy token')
+            click(location.left + 130, location.top + 10, clicks=2)
+            sleep(0.5)
 
-        hotkey('ctrl', 'c')
-        sleep(0.3)
+            hotkey('ctrl', 'c')
+            sleep(0.3)
 
-        firefox.kill()
-        token = paste()
-        if len(token) != 64:
-            l.error('invlid token %s', token)
-        else:
-            return token
+            hotkey('ctrl', 'shift', 'q')
+            token = paste()
+            if len(token) != 64:
+                l.error('invlid token %s', token)
+            else:
+                return token
     except Exception as e:
         l.error('error', e.__class__.__name__, e)
         return None
