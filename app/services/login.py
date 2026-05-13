38761a956645d018e4d6c0e74c815c22bbec7644
fi-res <nta16022013@gmail.com>
2026-05-13 19:28:48 +0300

    fix(login): try to fix screenshots

--- app/services/login.py
+++ app/services/login.py
@@ -1,5 +1,5 @@
 from time import sleep, time
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, run as sp_run
 from app.logger import get_logger
 
 from pyautogui import click, typewrite, hotkey, locateOnScreen as locate_on_screen, ImageNotFoundException
@@ -7,18 +7,22 @@ from pyperclip import paste
 
 l = get_logger('services.login')
 
-def wait_for_image(path: str, timeout: int = 30):
+def wait_for_image(path: str, timeout: int = 60):
     path = f'app/services/login_screens/{path}'
     start = time()
     while True:
-        sleep(0.2)
+        sleep(1)
         if time() - start > timeout:
             l.warning('%s not found after timeout', path)
             break
         try:
-            return locate_on_screen(path, grayscale=True)
+            l.debug('start locate')
+            result = locate_on_screen(path, grayscale=True)
+            l.debug('end locate')
+            if result is not None:
+                return result
         except ImageNotFoundException:
-            pass
+            l.debug('error locate')
 
 def login(email: str, password: str) -> str | None:
     # start browser
@@ -45,10 +49,11 @@ def login(email: str, password: str) -> str | None:
 
         l.debug('click login button')
         click(802, 621)
-        wait_for_image('captcha.png', 15)
 
+        wait_for_image('captcha.png')
         l.debug('click captcha')
         click(638, 440)
+
         assert wait_for_image('logout.png')
 
         l.debug('open devtools')
@@ -59,20 +64,24 @@ def login(email: str, password: str) -> str | None:
         click(769, 103)
         sleep(0.5)
 
-        if location := wait_for_image('token.png', 1):
-            l.debug('copy token')
-            click(location.left + 130, location.top + 10, clicks=2)
-            sleep(0.5)
+        l.debug('search token')
+        click(330, 124)
+        typewrite('refresh_token')
+        sleep(0.3)
 
-            hotkey('ctrl', 'c')
-            sleep(0.3)
+        l.debug('copy token')
+        click(401, 174, clicks=2)
+        sleep(0.3)
 
-            token = paste()
-            if len(token) != 64:
-                l.error('invlid token %s', token)
-            else:
-                return token
+        hotkey('ctrl', 'c')
+        sleep(0.3)
+
+        token = paste()
+        if len(token) != 64:
+            l.error('invlid token %s', token)
+        else:
+            return token
     except Exception as e:
         l.error('error %s %s', e.__class__.__name__, e)
     finally:
-        hotkey('ctrl', 'q')
+        sp_run(['pkill', '-SIGTERM', '-f', 'firefox'], capture_output=True)
