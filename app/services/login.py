4157754fc9601145e2c5343d91ce76ef58deea9c
fi-res <nta16022013@gmail.com>
2026-05-15 16:26:07 +0300

    perf(login): use login api and catch only turnstile

--- app/services/login.py
+++ app/services/login.py
@@ -2,6 +2,8 @@ from time import sleep, time
 from subprocess import Popen, PIPE, run as sp_run
 from app.logger import get_logger
 
+from itd import ITDClient
+from itd.api.auth import sign_in
 from pyautogui import click, typewrite, hotkey, locateOnScreen as locate_on_screen, ImageNotFoundException
 from pyperclip import paste
 
@@ -25,7 +27,7 @@ def wait_for_image(path: str, timeout: int = 10):
         sleep(3)
 
 
-def login(email: str, password: str) -> str | None:
+def get_turnstile() -> str | None:
     # start browser
     # click(887, 1049, button='RIGHT')
     # sleep(0.05)
@@ -43,53 +45,51 @@ def login(email: str, password: str) -> str | None:
             l.error('failed to connect')
             return None
 
-        # login
+        l.debug('open devtools')
+        hotkey('f12')
+        sleep(0.3)
+
+        l.debug('open console')
+        hotkey('ctrl', 'shift', 'k')
+        sleep(0.3)
+
+        l.debug('inject script')
+        typewrite('window.fetch = (input, init) => {console.log(init.body.turnstileToken)}')
+        sleep(0.5)
+
         l.debug('enter email')
-        click(680, 383)
-        typewrite(email)
+        click(530, 425)
+        typewrite('itd_sdk_login@gmail.com')
         sleep(0.1)
 
         l.debug('enter password')
-        click(680, 496)
-        typewrite(password)
+        click(530, 530)
+        typewrite('123')
         sleep(0.1)
 
         l.debug('click login button')
-        click(802, 621)
+        click(603, 659)
 
         wait_for_image('captcha.png')
         l.debug('click captcha')
-        click(638, 440)
+        click(480, 440)
 
-        assert wait_for_image('logout.png')
+        assert wait_for_image('home.png')
 
-        l.debug('open devtools')
-        hotkey('f12')
-        sleep(0.5)
-
-        l.debug('open storage tab')
-        click(769, 103)
-        sleep(0.5)
-
-        l.debug('search token')
-        click(330, 124)
-        typewrite('refresh_token')
-        sleep(0.3)
-
-        l.debug('copy token')
-        click(401, 174, clicks=2)
-        sleep(0.3)
-
+        l.debug('copy turnstile')
+        click(1288, 422, clicks=3)
         hotkey('ctrl', 'c')
-        sleep(0.3)
 
-        token = paste()
-        if len(token) != 64:
-            l.error('invlid token %s', token)
-        else:
-            return token
+        return eval(paste())['turnstileToken']
     except Exception as e:
         l.error('error %s %s', e.__class__.__name__, e)
     finally:
         l.info('spent %ss', round(time() - start))
         sp_run(['pkill', '-SIGTERM', '-f', 'firefox'])
+
+
+def login(email: str, password: str):
+    turnstile = get_turnstile()
+    if turnstile is None:
+        return
+    return sign_in(ITDClient(), email, password, turnstile).cookies.get('refresh_token')
--- main.py
+++ main.py
@@ -1,17 +1,18 @@
+from typing import Callable
 from pathlib import Path
-from os import getenv
 from datetime import datetime
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from fastapi.templating import Jinja2Templates
+from uvicorn import run
 
 from app.routers import router
 from app.routers.api import router as api_router
 from app.services.github_service import get_analogs, get_projects
 from app.services.db import create_db
-from app.logger import setup_logging
+from app.logger import setup_logging, get_logger, format_request
 
 BASE_DIR = Path(__file__).resolve().parent / "app"
 
@@ -19,6 +20,7 @@ app = FastAPI(docs_url=None, redoc_url=None)
 templates = Jinja2Templates(directory="app/templates/")
 create_db()
 setup_logging('DEBUG')
+l = get_logger()
 
 app.state.projects = get_projects()
 app.state.analogs = get_analogs()
@@ -37,6 +39,18 @@ app.include_router(router)
 app.include_router(api_router)
 
 
+@app.middleware("http")
+async def middleware(request: Request, call_next: Callable) -> Response:
+    response: Response = await call_next(request)
+    # custom access log
+    l.info(format_request(request, response), extra={"highlighter": None})
+    return response
+
+
 @app.get('/favicon.ico')
 def get_favicon():
     return FileResponse(BASE_DIR / 'static' / 'favicon.ico')
+
+
+run(app, host='127.0.0.1', port=8997, log_level='warning')
+
